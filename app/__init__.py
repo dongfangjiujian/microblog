ca7dbb92c6f51fd49701797bed1e764596d87a5f
ZuoXu <731933255@qq.com>
2018-06-14 10:43:44 +0800

    DateBase Register

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,10 @@ from config import config
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
-
+login = LoginManager()
 db= SQLAlchemy()
 migrate = Migrate()
 bootstrap = Bootstrap()
@@ -17,6 +18,9 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     migrate.init_app(app)
+    login.init_app(app)
+
+
     from .main import main as main_bp
     app.register_blueprint(main_bp)
 
--- app/auth/view.py
+++ app/auth/view.py
@@ -1,7 +1,20 @@
-from flask import redirect,url_for
+from flask import redirect,url_for,flash,render_template
 from . import auth
+from app.auth.form import RegisterForm
+from flask_login import current_user,login_user
+from app import db
+from app.models import User
 
-@auth.route('/index')
-def index():
-
-    return "auth index"
+@auth.route('/register',methods = ['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("You have registered successfully.s")
+        return redirect(url_for('main.index'))
+    return render_template('auth/register.html',title='Register',form=form)
